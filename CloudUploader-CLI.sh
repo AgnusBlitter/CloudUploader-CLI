ceeb6401d033d6a3ca676bb6e0f1cf9db48dec02
AgnusBlitter <agnusblitter@tuta.io>
2024-02-27 19:44:43 +0100

    Actualizacion de blogb storage para azure

--- CloudUploader-CLI.sh
+++ CloudUploader-CLI.sh
@@ -2,12 +2,45 @@
 
 # A script that quickly upload files to Azure
 
-# Azure Storage Account Information
-account_name="your_storage_account_name"
-account_key="your_storage_account_key"
-container_name="your_container_name"
+# File to Upload
+file_path=$1
 
-echo -n "enter the value : "
-read account_name
+# Check if file_path is provided
 
-echo "The value is : $account_name"
+if [ -z "$file_path" ]; then
+  echo "Missing argument"
+  echo "Usage: $0 <file_path>"
+  exit 1
+fi
+
+# Check if Azure CLI is installed
+if ! command -v az &> /dev/null; then
+    echo "Azure CLI is not installed. Please install it first."
+    exit 1
+else
+    # Gets Azure arguments from the user
+
+    echo -n "Plz enter account name : "
+    read storage_account
+    echo -n "Plz enter storage account key : "
+    read account_key
+    echo -n "Plz enter container account name : "
+    read container_name
+fi
+
+# Authenticate with Azure CLI
+az login
+
+# Upload the file to Azure Blob Storage
+az storage blob upload \
+  --account-name $storage_account \
+  --account-key $account_key \
+  --container-name $container_name \
+  --file $file_path
+
+# Check if upload was successful
+if [ $? -eq 0 ]; then
+  echo "File uploaded successfully to Azure Blob Storage."
+else
+  echo "Error: Failed to upload file to Azure Blob Storage."
+fi
